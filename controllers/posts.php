590dff3f62d4a1b9466632e0c52de03e1d7fd33c
katrin.sooaar <katrin.sooaar@khk.ee>
2019-01-10 09:37:42 +0200

    Add single post viewing functionality

--- controllers/posts.php
+++ controllers/posts.php
@@ -9,6 +9,13 @@ class posts extends Controller
      * be omitted (URL can be /). After calling the action, a view from views/controller-name/controller-name_action-name.php
      * is loaded (it must exist, unless the function ends with stop() call.
      */
+    // View function for blog posts
+    function view()
+    {
+        $post_id = $this->params[0];
+        $this->post = get_first("SELECT * FROM post NATURAL JOIN users WHERE post_id='$post_id'");
+    }
+
     function index()
     {
 
--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,13 +1,11 @@
 
-<?foreach ($posts as $post):?>
-<div class="span8">
-    <h1>Alice in Wonderland, part dos</h1>
-    <p>'You ought to be ashamed of yourself for asking such a simple question,' added the Gryphon; and then they both sat silent and looked at poor Alice, who felt ready to sink into the earth. At last the Gryphon said to the Mock Turtle, 'Drive on, old fellow! Don't be all day about it!' and he went on in these words:
-        'Yes, we went to school in the sea, though you mayn't believe it—'
-        'I never said I didn't!' interrupted Alice.
-        'You did,' said the Mock Turtle.</p>
-    <div>
-        <span class="badge badge-success">Posted 2012-08-02 20:47:04</span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
+<?foreach($posts as $post):?>
+    <div class="span8">
+        <h1><a href="<? echo BASE_URL?>posts/view/<?echo $post['post_id']?>"><?echo $post['post_subject']; ?></a></h1>
+        <p><? echo $post['post_text']?></p>
+        <div>
+            <span class="badge badge-success">Posted <?php echo $post['post_created']; ?></span><div class="pull-right"><span class="label label-primary">alice</span> <span class="label label-primary">story</span> <span class="label label-primary">blog</span> <span class="label label-primary">personal</span></div>
+        </div>
+        <hr>
     </div>
-</div>
 <?endforeach?>
